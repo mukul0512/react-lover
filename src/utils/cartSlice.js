fd86e407626c0b986bf0b2d3f3504ddbe68a764c
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-01-17 20:03:02 +0530

    immerjs/ mutate vs don't mutate state/ Vanilla redux vs redux toolkit

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+// import { current } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -7,14 +8,34 @@ const cartSlice = createSlice({
     },
     reducers: {
         addItem: (state, action) => {
+            // Warning by Vanilla Redux i.e. older redux: Don't mutate the state, returning was mandatory 
+            // const newState = [...state];
+            // newState.items.push(action.payload);
+            // return newState;
+
+
+            // Note: Redux toolkit uses immer js(a library) behind the scene.
             // mutating the state here
+            // Redux toolkit : We have to mutate the state
             state.items.push(action.payload);
         },
         removeItem: (state) => {
             state.items.pop();
         },
+        //originalState = {items: ["pizza"]}
+        // clearCart: (state) => {
+        //     console.log(state); // will not print [pizza] 
+        //     console.log(current(state)); // will now be print[pizza] 
+        //     state = []; // not possible to do that directly. Doesn't work there
+        //     console.log(state); // []  
+        // RTK - Either mutate the existing state or return a new state 
+        // state.items.length = 0; // state = [] i.e. originalState will be empty
+        //     return {items: []}; // this new object will be replaced inside originalState = {items: []}
+        // }
+
         clearCart: (state) => {
-            state.items.length = 0; // []
+            // state = ["Mukul"]; // not possible to do that directly. Doesn't work there
+            state.items.length = 0; // state = [] i.e. originalState will be empty
         }
     }
 });
