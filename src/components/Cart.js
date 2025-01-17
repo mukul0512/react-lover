af9d2e3745c729b22c21c72dcf2948206032ef9b
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-01-17 17:36:14 +0530

    subscribing to the store

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,7 +4,13 @@ import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
     //Subscribing to the store using useSelector hook
-    const cartItems = useSelector((store) => store.cart.items);
+
+    // Note: subscribing to the small portion of the store is the good way or efficient way.
+    const cartItems = useSelector((store) => store.cart.items);  // here we are subscribing to the small portion of the store using selector.
+
+    // The above and this is the same for subscribing to the store but but but ...
+    // const store = useSelector((store) => store); // here we are subscribing to the whole store but it is very less efficient
+    // const cartItems = store.cart.items; // here we are extracting the items. 
     console.log(cartItems);
 
     const dispatch = useDispatch()
@@ -18,9 +24,12 @@ const Cart = () => {
             <h1 className="text-2xl font-bold">Cart</h1>
             <div className="w-6/12 m-auto">
                 <button className="p-2 m-2 bg-black text-white rounded-lg"
-                onClick={handleClearCart}>
+                    onClick={handleClearCart}>
                     Clear Cart
                 </button>
+                {cartItems.length === 0 && (
+                    <h1>Cart is empty. Add items to the cart!</h1>
+                )}
                 <ItemList items={cartItems} />
             </div>
         </div>
